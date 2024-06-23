3af85bcf5529e2472857fcb24ed49b3b4b495421
risalim <42958749+risalim@users.noreply.github.com>
2024-06-23 17:08:04 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -9,8 +9,8 @@ with st.form("form") :
   urls = st.text_area(label="Enter URLs")
   if urls != "" :
     scrape_button = st.form_submit_button("Scrape")
-  if scrape_button :
-    for url in urls :
-      page = requests.get(URL)
-      soup = BeautifulSoup(page.content, "html.parser")
-      st.write("soup", soup)
+    if scrape_button :
+      for url in urls :
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, "html.parser")
+        st.write("soup", soup)
