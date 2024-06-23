107f925cdd025bef367a6e8891ff51f4db0f1de2
risalim <42958749+risalim@users.noreply.github.com>
2024-06-23 16:14:22 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
 import streamlit as st
-
+import requests
+from bs4 import BeautifulSoup
 
 st.title("NLP Service")
+st.text_area(label="Enter URLs")
+
+for url in urls :
+  page = requests.get(URL)
+  soup = BeautifulSoup(page.content, "html.parser")
+  st.write("soup", soup)
