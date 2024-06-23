7506b0cf5c8244abce921f0152941b97e07943a2
risalim <42958749+risalim@users.noreply.github.com>
2024-06-23 16:09:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
-from app.app import app
 
-st.set_page_config(
-    page_title="Web Scrapper",
-    layout="wide"
-)
 
-def main() :
-    with st.sidebar :
-        pages = {
-            "Web Scrapper": app
-        }
-
-if __name__ == "__main__" :
-    main()
+st.write("test")
